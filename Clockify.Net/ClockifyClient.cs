06a3110bf0f9a5598005159ae351bf87533653cc
Alex Zakharov <alexander.zakharov.113@gmail.com>
2020-03-14 02:50:17 +0300

    Change 12-hour time format to ISO 8601

--- Clockify.Net/ClockifyClient.cs
+++ Clockify.Net/ClockifyClient.cs
@@ -395,8 +395,8 @@ namespace Clockify.Net
 			var request = new RestRequest($"workspaces/{workspaceId}/user/{userId}/time-entries");
 
             if (description != null) { request.AddQueryParameter(nameof(description), description); }
-            if (start != null) { request.AddQueryParameter(nameof(start), start.Value.ToString("yyyy-MM-ddThh:mm:ssZ")); }
-			if (end != null) { request.AddQueryParameter(nameof(end), end.Value.ToString("yyyy-MM-ddThh:mm:ssZ")); }
+            if (start != null) { request.AddQueryParameter(nameof(start), start.Value.ToString("yyyy-MM-ddTHH:mm:ssZ")); }
+			if (end != null) { request.AddQueryParameter(nameof(end), end.Value.ToString("yyyy-MM-ddTHH:mm:ssZ")); }
             if (project != null) { request.AddQueryParameter(nameof(project), project); }
             if (task != null) { request.AddQueryParameter(nameof(task), task); }
             if (projectRequired != null) { request.AddQueryParameter("project-required", projectRequired.ToString()); }
