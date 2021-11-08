34d49325fc3b41dafaaf80b9b1fbcb3bb319462d
Morasiu <moras2601@wp.pl>
2021-11-08 17:30:13 +0100

    1.12.0

--- Clockify.Net/ClockifyClient.cs
+++ Clockify.Net/ClockifyClient.cs
@@ -10,7 +10,7 @@ namespace Clockify.Net
 {
     public partial class ClockifyClient
     {
-        private const string BaseUrl = "https://api.clockify.me/api/v1";
+        private const string ApiUrl = "https://api.clockify.me/api/v1";
         private const string ExperimentalApiUrl = "https://api.clockify.me/api/";
         private const string ReportsApiUrl = "https://reports.api.clockify.me/v1";
         private const string ApiKeyHeaderName = "X-Api-Key";
@@ -19,26 +19,29 @@ namespace Clockify.Net
         private IRestClient _experimentalClient;
         private IRestClient _reportsClient;
 
-        public ClockifyClient(string apiKey)
+        public ClockifyClient(string apiKey, string apiUrl = ApiUrl, string experimentalApiUrl = ExperimentalApiUrl, string reportsApiUrl = ReportsApiUrl)
         {
-            InitClients(apiKey);
+            InitClients(apiKey, apiUrl, experimentalApiUrl, reportsApiUrl);
         }
 
         /// <summary>
         /// Creates new <see cref="ClockifyClient"/>.
         /// Uses value from environment variable named "CAPI_KEY"
         /// </summary>
-        public ClockifyClient()
-        {
-            var apiKey = Environment.GetEnvironmentVariable(ApiKeyVariableName);
-            if (apiKey == null) { throw new ArgumentException($"Environment variable {ApiKeyVariableName} is not set."); }
-
-            InitClients(apiKey);
-        }
+        public ClockifyClient() : this(GetApiKeyFromEnv()) { }
 
         #region Private methods
 
-        private void InitClients(string apiKey)
+        private static string GetApiKeyFromEnv() {
+            var apiKey = Environment.GetEnvironmentVariable(ApiKeyVariableName);
+            if (apiKey == null) {
+                throw new ArgumentException($"Environment variable {ApiKeyVariableName} is not set.");
+            }
+
+            return apiKey;
+        }
+
+        private void InitClients(string apiKey, string apiUrl, string experimentalApiUrl, string reportsApi)
         {
             var jsonSerializerSettings = new JsonSerializerSettings()
             {
@@ -53,15 +56,15 @@ namespace Clockify.Net
                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
             };
 
-            _client = new RestClient(BaseUrl);
+            _client = new RestClient(apiUrl);
             _client.AddDefaultHeader(ApiKeyHeaderName, apiKey);
             _client.UseNewtonsoftJson(jsonSerializerSettings);
 
-            _experimentalClient = new RestClient(ExperimentalApiUrl);
+            _experimentalClient = new RestClient(experimentalApiUrl);
             _experimentalClient.AddDefaultHeader(ApiKeyHeaderName, apiKey);
             _experimentalClient.UseNewtonsoftJson(jsonSerializerSettings);
 
-            _reportsClient = new RestClient(ReportsApiUrl);
+            _reportsClient = new RestClient(reportsApi);
             _reportsClient.AddDefaultHeader(ApiKeyHeaderName, apiKey);
             _reportsClient.UseNewtonsoftJson(jsonSerializerSettings);
         }
