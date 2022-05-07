5652b6bd60959a969eca295604bdb5aa9ddcf844
Morasiu <moras2601@wp.pl>
2022-05-07 11:19:19 +0200

    Delete tag async

--- Clockify.Net/ClockifyClient.Tags.cs
+++ Clockify.Net/ClockifyClient.Tags.cs
@@ -19,7 +19,7 @@ namespace Clockify.Net
         }
 
         /// <summary>
-        /// Add a new client to workspace.
+        /// Add a new tag to workspace.
         /// </summary>
         public async Task<Response<TagDto>> CreateTagAsync(string workspaceId, TagRequest projectRequest)
         {
@@ -29,6 +29,18 @@ namespace Clockify.Net
             var request = new RestRequest($"workspaces/{workspaceId}/tags", Method.Post);
             request.AddJsonBody(projectRequest);
             return Response<TagDto>.FromRestResponse(await _client.ExecutePostAsync<TagDto>(request).ConfigureAwait(false));
+        }
+
+        
+        /// <summary>
+        /// Delete tag from workspace.
+        /// </summary>
+        /// <param name="workspaceId"></param>
+        /// <param name="tagId"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<Response> DeleteTagAsync(string workspaceId, string tagId) {
+            var request = new RestRequest($"workspaces/{workspaceId}/tags/{tagId}", Method.Delete);
+            return Response.FromRestResponse(await _client.DeleteAsync(request).ConfigureAwait(false));
         }
     }
 }
