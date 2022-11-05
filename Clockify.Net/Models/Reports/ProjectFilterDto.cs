93f9bbf481bd0ce46acbc9644fe038f28d80325f
Mariusz Kogut <mk@handamde-systems.de>
2022-11-05 07:14:07 +0100

    feat: add project filter to detailed report

---
+++ Clockify.Net/Models/Reports/ProjectFilterDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Clockify.Net.Models.Reports
+{
+    public class ProjectFilterDto
+    {
+        public ContainsType Contains { get; set; }
+        public List<string> Ids { get; set; }
+        public StatusType Status { get; set; }
+    }
+}
