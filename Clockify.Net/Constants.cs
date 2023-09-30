021b83741957c618771e8066703f164bc08504e3
Turner Bass <turner@astropanda.studio>
2023-09-30 18:49:41 -0500

    Beginning project restructure

---
+++ Clockify.Net/Constants.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Clockify.Net;
+
+/// <summary>
+/// Constants used within the Clockify.Net SDK
+/// </summary>
+public class Constants
+{
+    /// <summary>
+    /// The base URL of the core API
+    /// </summary>
+    public const string ApiUrl = "https://api.clockify.me/api/v1";
+
+    /// <summary>
+    /// The base URL of the Experimental API
+    /// </summary>
+    public const string ExperimentalApiUrl = "https://api.clockify.me/api/";
+
+    /// <summary>
+    /// The base URL of the Reports API
+    /// </summary>
+    public const string ReportsApiUrl = "https://reports.api.clockify.me/v1";
+
+    /// <summary>
+    /// The base URL of the PTO API
+    /// </summary>
+    public const string PTOApiUrl = "https://pto.api.clockify.me/v1";
+
+    /// <summary>
+    /// The name of the API Key header for authenticating requests against Clockify APIs
+    /// </summary>
+    public const string ApiKeyHeaderName = "X-Api-Key";
+
+    /// <summary>
+    /// The name of the environment variable for the Clockify API Key value
+    /// </summary>
+    public const string ApiKeyVariableName = "CAPI_KEY";
+
+    /// <summary>
+    /// The configuration key for <see cref="IConfiguration"/> access of the API Key value
+    /// </summary>
+    public const string ApiKeyConfigurationKey = "Clockify:ApiKey";
+
+    /// <summary>
+    /// The name of the core Clockify HTTP client
+    /// </summary>
+    public const string ClockifyClientName = "Clockify";
+
+    /// <summary>
+    /// The name of the Experimental Clockify HTTP client
+    /// </summary>
+    public const string ExperimentalClientName = "ClockifyExperimental";
+
+    /// <summary>
+    /// The name of the Reports Clockify HTTP client
+    /// </summary>
+    public const string ReportsClientName = "ClockifyReports";
+
+    /// <summary>
+    /// The name of the PTO Clockify HTTP client
+    /// </summary>
+    public const string PTOClientName = "ClockifyPTO";
+}
---
+++ Clockify.Net/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Clockify.Net;
+using Clockify.Net.Clients;
+using Clockify.Net.Exceptions;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extensions for dynamically configuration the Clockify clients.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds the Clockify Client to the Service Collection
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidClockifyConfigurationException"></exception>
+    public static IServiceCollection AddClockifyClient(this IServiceCollection services)
+    {
+        var sp = services.BuildServiceProvider();
+        var config = sp.GetRequiredService<IConfiguration>();
+
+        string apiKey = config.GetValue<string>(Constants.ApiKeyConfigurationKey) ?? string.Empty;
+        string legacyApiKeyVariable = Environment.GetEnvironmentVariable(Constants.ApiKeyVariableName);
+
+        string finalApiKey = string.IsNullOrWhiteSpace(apiKey) ? legacyApiKeyVariable : apiKey;
+
+        if (string.IsNullOrWhiteSpace(finalApiKey))
+            throw new InvalidClockifyConfigurationException($"Cannot a configuration value for the Environment Variable {Constants.ApiKeyVariableName} or the config key {Constants.ApiKeyConfigurationKey}");
+
+
+        services.AddTransient<IClockifyClient, ClockifyClient>();
+
+
+        services.AddHttpClient<IClockifyExperimentalClient>(Constants.ExperimentalClientName, httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(Constants.ExperimentalApiUrl);
+            httpClient.DefaultRequestHeaders.Add(Constants.ApiKeyHeaderName, finalApiKey);
+        });
+
+        services.AddHttpClient<IClockifyReportsClient>(Constants.ReportsClientName, httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(Constants.ReportsApiUrl);
+            httpClient.DefaultRequestHeaders.Add(Constants.ApiKeyHeaderName, finalApiKey);
+        });
+
+        services.AddHttpClient<IClockifyPTOClient>(Constants.PTOClientName, httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(Constants.PTOApiUrl);
+            httpClient.DefaultRequestHeaders.Add(Constants.ApiKeyHeaderName, finalApiKey);
+        });
+
+        services.AddHttpClient<IClockifyClient>(Constants.ClockifyClientName, httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(Constants.ApiUrl);
+            httpClient.DefaultRequestHeaders.Add(Constants.ApiKeyHeaderName, finalApiKey);
+        });
+
+        return services;
+    }
+}
