e74ad85a6a081c20043ad6d36594cae321397a1c
GamiDroid <kevin.d.damen@gmail.com>
2024-06-01 17:21:49 +0200

    add missing client dto properties

--- Clockify.Net/Models/Clients/ClientDto.cs
+++ Clockify.Net/Models/Clients/ClientDto.cs
@@ -5,5 +5,11 @@
         public string? Id { get; set; }
         public string? Name { get; set; }
         public string? WorkspaceId { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? CurrencyCode { get; set; }
+        public string? CurrencyId { get; set; }
+        public string? Note { get; set; }
+        public bool Archived { get; set; }
     }
 }
