4224010bc560c1b117433973c13c68ec41d69c3f
Thies Reinhold <65960018+thiesitease@users.noreply.github.com>
2025-03-25 15:37:59 +0100

    Usergroup access added

---
+++ Clockify.Net/ClockifyClient.Groups.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Clockify.Net.Models;
+using Clockify.Net.Models.Users;
+using RestSharp;
+
+namespace Clockify.Net
+{
+    public partial class ClockifyClient
+    {
+        /// <summary>
+        /// Find all users on workspace
+        /// </summary>
+        public async Task<Response<List<UserGroupDto>>> FindAllGroupsOnWorkspaceAsync(string workspaceId, int page = 1, int pageSize = 50)
+        {
+            var request = new RestRequest($"workspaces/{workspaceId}/user-groups");
+
+            request.AddQueryParameter(nameof(page), page.ToString());
+            request.AddQueryParameter("page-size", pageSize.ToString());
+
+            return Response<List<UserGroupDto>>.FromRestResponse(await _client.ExecuteGetAsync<List<UserGroupDto>>(request).ConfigureAwait(false));
+        }
+
+    }
+}
---
+++ Clockify.Net/Models/Group/UserGroupDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using Clockify.Net.Models.Memberships;
+
+namespace Clockify.Net.Models.Users
+{
+    public class UserGroupDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? WorkspaceId { get; set; }
+        public List<string> UserIds { get; set; } = [];
+    }
+}
